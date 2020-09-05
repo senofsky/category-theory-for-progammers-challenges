672dcdb2f16e3e4e952fb8b25e81aa051d352795
Mark V. Senofsky <mark.v.senofsky@gmail.com>
2020-09-05 17:24:34 -0500

    Add identity function in Python

---
+++ identity_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Challenge 1: Identity function in Python
+
+def id(x):
+    return x
+
+
+if __name__ == "__main__":
+    print(f"id(42) = {id(42)}")
+    print(f'id("hello world!") = {id("hello world!")}')
